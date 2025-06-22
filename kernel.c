0b19913093da37b2e3627d2e1ec042f59be6e2c3
Sanira Deneth <sanira.adesha@gmail.com>
2025-06-22 20:23:40 +0530

    Fixed cursor error and clear screen error

--- kernel.c
+++ kernel.c
@@ -3,6 +3,8 @@
 #define VGA_CTRL 0x3D4
 #define VGA_DATA 0x3D5
 
+// Global cursor position tracker
+static int current_cursor_pos = 0;
 static volatile char *vidmem = (volatile char *)VIDEO_MEMORY;
 
 // Function declarations
@@ -11,17 +13,30 @@ void __attribute__((section(".text"))) print_string(const char *str);
 void __attribute__((section(".text"))) clear_screen();
 void __attribute__((section(".text"))) get_input(char *buffer);
 void __attribute__((section(".text"))) print_system_info();
+void __attribute__((section(".text"))) update_cursor(int x, int y);
 int __attribute__((section(".text"))) strcmp(const char *s1, const char *s2);
 
-// Simple string comparison
-int strcmp(const char *s1, const char *s2)
+// I/O port functions
+static inline unsigned char inb(unsigned short port)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
-    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    unsigned char ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+static inline void outb(unsigned short port, unsigned char val)
+{
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+// Update hardware cursor position
+void update_cursor(int x, int y)
+{
+    unsigned short pos = y * 80 + x;
+    outb(VGA_CTRL, 0x0F);
+    outb(VGA_DATA, (unsigned char)(pos & 0xFF));
+    outb(VGA_CTRL, 0x0E);
+    outb(VGA_DATA, (unsigned char)((pos >> 8) & 0xFF));
 }
 
 // Keyboard scancode to ASCII mapping (US QWERTY)
@@ -59,17 +74,15 @@ static const unsigned char keyboard_map[128] = {
     0  /* All other keys are undefined */
 };
 
-// I/O port functions
-static inline unsigned char inb(unsigned short port)
+// Simple string comparison
+int strcmp(const char *s1, const char *s2)
 {
-    unsigned char ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-
-static inline void outb(unsigned short port, unsigned char val)
-{
-    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+    while (*s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
 // Clear the screen
@@ -80,61 +93,63 @@ void clear_screen()
         vidmem[i] = ' ';
         vidmem[i + 1] = WHITE_ON_BLACK;
     }
-    print_string("DA-Os By Sanira Deneth v0.1\n");
+    current_cursor_pos = 0;
+    update_cursor(0, 0);
+    print_string("==> Welcome to DA-Os <==\n\n");
 }
 
 // Print a string to screen
 void print_string(const char *str)
 {
-    static int cursor_pos = 0;
-
     while (*str)
     {
         if (*str == '\n')
         {
-            cursor_pos += (80 - (cursor_pos % 80));
+            current_cursor_pos += (80 - (current_cursor_pos % 80));
         }
         else
         {
-            vidmem[cursor_pos * 2] = *str;
-            vidmem[cursor_pos * 2 + 1] = WHITE_ON_BLACK;
-            cursor_pos++;
+            vidmem[current_cursor_pos * 2] = *str;
+            vidmem[current_cursor_pos * 2 + 1] = WHITE_ON_BLACK;
+            current_cursor_pos++;
         }
         str++;
 
-        // Handle scrolling
-        if (cursor_pos >= 80 * 25)
+        if (current_cursor_pos >= 80 * 25)
         {
-            // Move all lines up
+            // Scroll screen up
             for (int i = 80 * 2; i < 80 * 25 * 2; i++)
             {
                 vidmem[i - 80 * 2] = vidmem[i];
             }
-            // Clear last line
+            // Clear new line
             for (int i = (80 * 24 * 2); i < 80 * 25 * 2; i += 2)
             {
                 vidmem[i] = ' ';
                 vidmem[i + 1] = WHITE_ON_BLACK;
             }
-            cursor_pos = 80 * 24;
+            current_cursor_pos = 80 * 24;
         }
     }
+    update_cursor(current_cursor_pos % 80, current_cursor_pos / 80);
 }
 
 // Get keyboard input
 void get_input(char *buffer)
 {
     int pos = 0;
+    int input_start_pos = current_cursor_pos;
+
     while (1)
     {
-        // Wait for keyboard input
+        // Wait for key press
         while ((inb(0x64) & 0x01) == 0)
             ;
 
         unsigned char key = inb(0x60);
 
         if (key == 0x1C)
-        { // Enter key
+        { // Enter
             buffer[pos] = 0;
             print_string("\n");
             return;
@@ -144,7 +159,9 @@ void get_input(char *buffer)
             if (pos > 0)
             {
                 pos--;
-                print_string("\b \b");
+                current_cursor_pos--;
+                vidmem[current_cursor_pos * 2] = ' ';
+                update_cursor(current_cursor_pos % 80, current_cursor_pos / 80);
             }
         }
         else if (key < 0x80)
@@ -153,8 +170,10 @@ void get_input(char *buffer)
             if (c >= 32 && c <= 126)
             {
                 buffer[pos++] = c;
-                char str[2] = {c, 0};
-                print_string(str);
+                vidmem[current_cursor_pos * 2] = c;
+                vidmem[current_cursor_pos * 2 + 1] = WHITE_ON_BLACK;
+                current_cursor_pos++;
+                update_cursor(current_cursor_pos % 80, current_cursor_pos / 80);
             }
         }
     }
@@ -163,18 +182,21 @@ void get_input(char *buffer)
 // Print system information
 void print_system_info()
 {
-    print_string("DA-Os v0.1\n");
-    print_string("Booted from floppy disk\n");
-    print_string("Video memory: 0xB8000\n");
-    print_string("Running in 16-bit real mode\n");
+    print_string("\n** System Information **\n");
+    print_string("Os: DA-Os v0.1\n");
+    print_string("Mode: 16-bit Real Mode\n");
+    print_string("Display: 80x25 Text Mode\n");
+    print_string("Video Memory: 0xB8000\n");
+    print_string("Boot Device: Floppy Disk\n\n");
 }
 
 // Kernel entry point
 void kernel_main()
 {
     clear_screen();
-    print_string("Type 'info' for system info, 'clear' to clear screen\n");
-    print_string("Type 'help' for command list\n\n");
+    print_string("Type 'info' for system information\n");
+    print_string("Type 'clear' to clear the screen\n");
+    print_string("Type 'help' for available commands\n\n");
 
     char input[32];
     while (1)
